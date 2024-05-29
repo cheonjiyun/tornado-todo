cc3767f75e41eddccf6036b9e83823b0e0e49d47
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-05-29 11:39:20 +0900

    feat: 카테고리 보기에서 체크하기 기능

--- src/component/cateogory/CategoryGroup.tsx
+++ src/component/cateogory/CategoryGroup.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { todosRecoil } from "../../recoil/todos/atom";
 import { useRecoilState } from "recoil";
+import { TodoType } from "../../type/todo";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
 type PropsType = {
     category: string;
@@ -11,12 +14,23 @@ export const CategoryGroup = ({ category }: PropsType) => {
     const [isOpen, setOpen] = useState(false);
     const [todos] = useRecoilState(todosRecoil);
 
-    const [categoryTodos, setCategoryTodos] = useState<string[] | undefined>([]);
+    const [categoryTodos, setCategoryTodos] = useState<TodoType[]>([]);
     useEffect(() => {
         const todoFilters = todos.filter((todo) => todo.category === category);
 
-        setCategoryTodos(todoFilters.map((todo) => todo.text));
+        setCategoryTodos(todoFilters);
     }, [todos]);
+
+    // check
+    const toggleCheckTodo = async (id: number, currentCompleted: boolean) => {
+        try {
+            await updateDoc(doc(db, "todos", `${id}`), {
+                completed: !currentCompleted,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
     return (
         <Group>
@@ -42,17 +56,63 @@ export const CategoryGroup = ({ category }: PropsType) => {
             </CategoryTitle>
             {isOpen && (
                 <TodoListGroup>
-                    {categoryTodos ? categoryTodos.map((todo) => <TodoList>{todo}</TodoList>) : ""}
+                    {categoryTodos
+                        ? categoryTodos.map((todo) => (
+                              <TodoList>
+                                  <CheckBox
+                                      $completed={todo.completed}
+                                      onClick={() => toggleCheckTodo(todo.id, todo.completed)}
+                                  >
+                                      {todo.completed && (
+                                          <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              width="20"
+                                              height="20"
+                                              viewBox="0 0 24 24"
+                                          >
+                                              <path
+                                                  fill="none"
+                                                  stroke="currentColor"
+                                                  strokeLinecap="round"
+                                                  strokeLinejoin="round"
+                                                  strokeWidth="2"
+                                                  d="M20 7L10 17l-5-5"
+                                              />
+                                          </svg>
+                                      )}
+                                  </CheckBox>{" "}
+                                  <TodoText $completed={todo.completed}>{todo.text}</TodoText>
+                              </TodoList>
+                          ))
+                        : ""}
                 </TodoListGroup>
             )}
         </Group>
     );
 };
 
+type completedType = {
+    $completed: boolean;
+};
+
 const Group = styled.div`
     margin: 8px;
     padding: 16px;
     background-color: #ffffff;
+`;
+
+const CheckBox = styled.div<completedType>`
+    width: 20px;
+    height: 20px;
+    border: 1px solid #8e8e8e;
+
+    cursor: pointer;
+
+    &:active {
+        transform: scale(90%);
+    }
+
+    ${(props) => props.$completed && "color: #b3b3b3; border: 1px solid #b3b3b3; "};
 `;
 const CategoryTitle = styled.div`
     display: flex;
@@ -72,7 +132,7 @@ const Arrow = styled.div<ArrowType>`
     justify-content: center;
     align-items: center;
 
-    transform: ${(props) => props.$isOpen || "rotate(180deg)"};
+    transform: ${(props) => (props.$isOpen ? "rotate(180deg)" : "rotate(0deg)")};
 
     cursor: pointer;
 `;
@@ -82,7 +142,16 @@ const TodoListGroup = styled.div`
 `;
 
 const TodoList = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
     margin: 8px;
     padding-top: 8px;
     padding-bottom: 8px;
+`;
+
+const TodoText = styled.div<completedType>`
+    font-size: 16px;
+
+    ${(props) => props.$completed && "text-decoration: line-through; color: #b3b3b3"};
 `;
